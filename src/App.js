3df8b6dcbc9a48a97a566108eebb948b9b670474
Fanel Secara <fanelsecara@gmail.com>
2021-09-15 13:42:52 +0200

    Change the hole hex generator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import './App.css';
+import './index.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { useState } from 'react';
+import Button from './Button.js';
+import Input from './Input.js';
 
 // App Comonent
 
 function App() {
   const [hex, setHex] = useState('#32a848');
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
 
   // Create random  Hex
-  const randomHex = () => {
+  /* const randomHex = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
     setHex(randomColor);
-  };
+  }; */
+
+  // onChange Handlers
+  function onChangeHue(event) {
+    setHue(event.currentTarget.value);
+  }
+
+  function onChangeLuminosity(event) {
+    setLuminosity(event.currentTarget.value);
+  }
+
+  const container = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  `;
 
   return (
     <div
+      css={container}
       className="App"
       style={{
         backgroundColor: hex,
@@ -24,7 +47,23 @@ function App() {
       }}
     >
       <h1>{hex}</h1>
-      <button onClick={randomHex}>Random Color</button>
+
+      <Input
+        text="luminosity"
+        type="text"
+        placeholder="Choose Luminosity "
+        value={luminosity}
+        onChange={onChangeLuminosity}
+      />
+      <Input
+        text="hue"
+        type="text"
+        placeholder="Choose Hue"
+        value={hue}
+        onChange={onChangeHue}
+      />
+
+      <Button setRandomColor={setHex} luminosity={luminosity} hue={hue} />
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,28 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import color from 'randomcolor';
+
+// CSS-in-JS
+const button = css`
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+  font-size: 16px;
+  width: 173px;
+  margin-top: 10px;
+`;
+
+export default function Button(props) {
+  return (
+    <button
+      css={button}
+      onClick={() =>
+        props.setRandomColor(
+          color.randomColor({ luminosity: props.luminosity, hue: props.hue }),
+        )
+      }
+    >
+      Random Color
+    </button>
+  );
+}
