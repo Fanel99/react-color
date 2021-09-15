fc0c444a6878839c83e7d8435e7b15e4852b3c9a
Fanel Secara <fanelsecara@gmail.com>
2021-09-15 14:11:27 +0200

    Input modifiy

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,6 @@ export default function Input(props) {
   return (
     <div>
       <input
-        type={props.type}
         placeholder={props.placeholder}
         value={props.value}
         onChange={props.onChange}
